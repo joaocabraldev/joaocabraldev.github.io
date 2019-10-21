0fa1f37b066b08d4545301f8064f75da3e563651
Joao Antonio <joaoantonio@Megasoft.local>
2019-10-21 13:50:11 -0300

    PWA offline e novo visual sem framework

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,13 +1,31 @@
+const version = "0.0.1";
+const cacheName = `appcache-${version}`;
+
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('appcache_0.01').then(function(cache) {
+   caches.open(cacheName).then(function(cache) {
      return cache.addAll([
        '/pwa',
        '/pwa/index.html',
        '/pwa/app.js',
        '/pwa/styles.css',
        '/pwa/images'
-     ]);
+     ])
+     .then(() => self.skipWaiting());
    })
  );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, {ignoreSearch: true}))
+            .then(response => {
+                return response || fetch(event.request);
+        })
+  );
 });
