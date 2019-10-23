7b74ea7d1342336e4b27373bcbe7342a0d1eec43
Joao Antonio <joaoantonio@Megasoft.local>
2019-10-23 09:46:34 -0300

    Ajustes Label

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,20 +1,22 @@
-const version = "0.0.1";
+const version = "0.0.2";
 const cacheName = `appcache-${version}`;
 
 self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open(cacheName).then(function(cache) {
-     return cache.addAll([
-       '/pwa',
-       '/pwa/index.html',
-       '/pwa/app.js',
-       '/pwa/styles.css',
-       '/pwa/images/calc_144.png',
-      '/pwa/images/calc_512.png'
-     ])
-     .then(() => self.skipWaiting());
-   })
- );
+	e.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			
+			return cache.addAll([
+				'/pwa',
+				'/pwa/favicon.ico',
+				'/pwa/index.html',
+				'/pwa/app.js',
+				'/pwa/styles.css',
+				'/pwa/images/calc_144.png',
+				'/pwa/images/calc_512.png'
+			])
+			.then(() => self.skipWaiting());
+		})
+	);
 });
 
 self.addEventListener('activate', event => {
@@ -24,9 +26,9 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.open(cacheName)
-            .then(cache => cache.match(event.request, {ignoreSearch: true}))
-            .then(response => {
-                return response || fetch(event.request);
-        })
-  );
+		.then(cache => cache.match(event.request, {ignoreSearch: true}))
+		.then(response => {
+			return response || fetch(event.request);
+		})
+	);
 });
