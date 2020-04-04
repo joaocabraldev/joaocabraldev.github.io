9448a1341970ff9ebbaafabc5428f855dca4c64a
João Antônio Cabral <joaomunizshura@gmail.com>
2020-04-04 13:53:51 -0300

    Novos cálculos

--- calculadora_inss_pwa/js/app.js
+++ calculadora_inss_pwa/js/app.js
@@ -1,3 +1,5 @@
+/* global visual, calculo */
+
 /**
  * Constante do aplicativo.
  * Contém os métodos para cálculo do INSS.
@@ -49,7 +51,7 @@ const app = {
       deferredPrompt = e;
       installBtn.style.display = 'block';
 
-      installBtn.addEventListener('click', (e) => {
+      installBtn.addEventListener('click', () => {
         installBtn.style.display = 'none';
         deferredPrompt.prompt();
         deferredPrompt.userChoice.then((choiceResult) => {
@@ -64,6 +66,9 @@ const app = {
     });
   },
 
+  /**
+   * Prepara o input para receber tecla Enter.
+   */
   preparaInputText: () => {
     const txtSalario = document.getElementById('txtSalario');
     txtSalario.addEventListener('keyup', (e) => {
